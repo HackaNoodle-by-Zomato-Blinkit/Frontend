cdd324883ebf35bfca1a368e0489d6fa9aff77c6
dragonman164 <sanidhiyafirefox123@gmail.com>
2022-11-06 10:51:40 +0530

    check

--- src/pages/Student/Quizes/RapidReadingTest.js
+++ src/pages/Student/Quizes/RapidReadingTest.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar2 from '../../../components/NavBar2';
-import {Box, Grid,Paper,Container} from '@mui/material';
+import {Box, Grid,Paper,Item,Container} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Fab from '@mui/material/Fab';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
@@ -9,34 +9,34 @@ import { useState, useEffect, useRef } from "react";
 import axios from 'axios';
 
 
-// const Item = styled(Paper)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(0),
-//     textAlign: 'center',
-//     height : '60px',
-//     fontSize: '50px',
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(0),
+    textAlign: 'center',
+    height : '60px',
+    fontSize: '50px',
     
-// }));
+}));
 
-// const style = {
-//     margin: 0,
-//     top: 'auto',
-//     right: 20,
-//     bottom: 20,
-//     left: 'auto',
-//     position: 'fixed',
-//     width : '100px',
-//     height : '100px'
-// };
+const style = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+    width : '100px',
+    height : '100px'
+};
 
 
-// let color = ['In recent years, handwritten digits recognition has been an important area due to its applications in several fields. This work is focusing on the recognition part of handwritten Arabic digits recognition that face several challenges, including the unlimited variation in human handwriting and the large public databases. The paper provided a deep',
-// 'State-of-the-art offline handwriting text recognition systems tend to use neural networks and therefore require a large amount of annotated data to be trained. In order to partially satisfy this requirement, we propose a system based on Generative Adversarial Networks (GAN) to produce synthetic images of handwritten words. We use bidirectional LSTM',
-// 'Recognition of handwritten digits has been an important area in recent years because of its uses in many fields. Arabic pattern digits, weak work is performed because Arabic digits (Indian) are more complicated than English patterns. This study focuses on the recognition component of the recognition of handwritten Arabic digits (Indian) that faces.',
-// 'Arabic handwriting is slightly different from the handwriting of other languages; hence it is possible to distinguish the handwriting written by the native or non-native writer based on their handwriting. However, classifying Arabic handwriting is challenging using traditional text recognition algorithms. Thus, this study evaluated and validated the end.',
-// 'The success of handwriting recognition methods based on digitizer-pen signal processing is mostly dependent on the defined features. The strong and discriminating feature descriptors can play a main role in improving the accuracy of pattern recognition. Moreover, most recognition studies utilize the local features or a sequence',
-// 'Deep learning has increased the performance of classification and object detection, but it generally requires large amounts of labeled data for training. In this paper, we introduce a new data augmentation algorithm that promotes diversity between classes, representing the characters of the Arabic script, and can balance samples']
+let color = ['In recent years, handwritten digits recognition has been an important area due to its applications in several fields. This work is focusing on the recognition part of handwritten Arabic digits recognition that face several challenges, including the unlimited variation in human handwriting and the large public databases. The paper provided a deep',
+'State-of-the-art offline handwriting text recognition systems tend to use neural networks and therefore require a large amount of annotated data to be trained. In order to partially satisfy this requirement, we propose a system based on Generative Adversarial Networks (GAN) to produce synthetic images of handwritten words. We use bidirectional LSTM',
+'Recognition of handwritten digits has been an important area in recent years because of its uses in many fields. Arabic pattern digits, weak work is performed because Arabic digits (Indian) are more complicated than English patterns. This study focuses on the recognition component of the recognition of handwritten Arabic digits (Indian) that faces.',
+'Arabic handwriting is slightly different from the handwriting of other languages; hence it is possible to distinguish the handwriting written by the native or non-native writer based on their handwriting. However, classifying Arabic handwriting is challenging using traditional text recognition algorithms. Thus, this study evaluated and validated the end.',
+'The success of handwriting recognition methods based on digitizer-pen signal processing is mostly dependent on the defined features. The strong and discriminating feature descriptors can play a main role in improving the accuracy of pattern recognition. Moreover, most recognition studies utilize the local features or a sequence',
+'Deep learning has increased the performance of classification and object detection, but it generally requires large amounts of labeled data for training. In this paper, we introduce a new data augmentation algorithm that promotes diversity between classes, representing the characters of the Arabic script, and can balance samples']
 
 let check = ""
 let data = []
@@ -51,7 +51,7 @@ function playSound(url) {
 
 
 
-// const RapidReadingTest = () => {
+const RapidReadingTest = () => {
 
     // const [timer, setTimer] = useState(30)
     const [rec,setRec] = useState(false)
@@ -196,11 +196,9 @@ function playSound(url) {
                 setRec(!rec)
                 
                
-// }} sx={style} color={rec?"error":"primary"} aria-label="add">
-//         {!rec?<KeyboardVoiceIcon sx={{width : '40px',height : '40px'}}/>:<StopIcon sx={{width : '40px',height : '40px'}}/>}
-//       </Fab>
+}} sx={style} color={rec?"error":"primary"} aria-label="add">         {!rec?<KeyboardVoiceIcon sx={{width : '40px',height : '40px'}}/>:<StopIcon sx={{width : '40px',height : '40px'}}/>}
+   </Fab>
       
-<<<<<<< HEAD
 
 <div>
         <div>
@@ -221,10 +219,3 @@ function playSound(url) {
 
 }
 export default RapidReadingTest;
-=======
-//          </>
-
-//     )
-// }
-// export default RapidReadingTest;
->>>>>>> 3f7b078f1e528dbe515f1b29bbe969ba6ddb9bf1
